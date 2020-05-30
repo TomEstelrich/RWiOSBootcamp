de08361cb102abf133a8030d7df64a85727fad12
Bartolome Estelrich <tom@estelrichapplabs.com>
2020-05-30 19:22:16 -0400

    Set Color button completed
    The Set Color button is working as expected for RGB.

--- BullsEye/Controllers/ColorPickerViewController.swift
+++ BullsEye/Controllers/ColorPickerViewController.swift
@@ -5,19 +5,40 @@ import UIKit
 class ColorPickerViewController: UIViewController {
   
   @IBOutlet weak var backgroundView: UIView!
+  @IBOutlet weak var colorNameLabel: UILabel!
+  
   @IBOutlet weak var redValueLabel: UILabel!
   @IBOutlet weak var greenValueLabel: UILabel!
   @IBOutlet weak var blueValueLabel: UILabel!
   
+  @IBOutlet weak var redSlider: UISlider!
+  @IBOutlet weak var greenSlider: UISlider!
+  @IBOutlet weak var blueSlider: UISlider!
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    configureSlidersForRGB()
+  }
+  
+  
+  @IBAction func redSliderDidDrag(_ sender: UISlider) {
+    redValueLabel.text = "\(Int(redSlider.value.rounded()))"
+  }
+  
+  
+  @IBAction func greenSliderDidDrag(_ sender: UISlider) {
+    greenValueLabel.text = "\(Int(greenSlider.value.rounded()))"
+  }
+  
+  
+  @IBAction func blueSliderDidDrag(_ sender: UISlider) {
+    blueValueLabel.text = "\(Int(blueSlider.value.rounded()))"
   }
   
   
   @IBAction func setColorButtonDidTap(_ sender: UIButton) {
-    print("Set Color button tapped.")
+    backgroundView.backgroundColor = createRGBColor()
   }
   
   
@@ -29,11 +50,43 @@ class ColorPickerViewController: UIViewController {
     redValueLabel.text = String(0)
     greenValueLabel.text = String(0)
     blueValueLabel.text = String(0)
+    
+    redSlider.value = 0
+    blueSlider.value = 0
+    greenSlider.value = 0
   }
   
   
   @IBAction func infoButtonDidTap(_ sender: UIButton) {
     print("Info button tapped.")
+  }
+  
+  
+  func createRGBColor() -> UIColor {
+    let userSelectedColor = UIColor(red: convertToRGBFromSlider(value: redSlider.value),
+                                    green: convertToRGBFromSlider(value: blueSlider.value),
+                                    blue: convertToRGBFromSlider(value: greenSlider.value),
+                                    alpha: 1.0)
+    return userSelectedColor
+  }
+  
+  
+  func convertToRGBFromSlider(value: Float) -> CGFloat {
+    let roundedValue = CGFloat(value.rounded())
+    let convertedValue = roundedValue / 255
+    return convertedValue
+  }
+  
+  
+  func configureSlidersForRGB() {
+    redSlider.minimumValue = 0
+    redSlider.maximumValue = 255
+    
+    greenSlider.minimumValue = 0
+    greenSlider.maximumValue = 255
+    
+    blueSlider.minimumValue = 0
+    blueSlider.maximumValue = 255
   }
   
 }
