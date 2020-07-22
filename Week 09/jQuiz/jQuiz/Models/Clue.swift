d9e448a133eb25189fc6e52fa8d771d48679ee86
Bartolome Estelrich <tom@estelrichapplabs.com>
2020-07-22 17:55:56 -0400

    Clue and Category properties created
    According to the JSON response for random category both Clue and Category structs have been defined.

--- Week 09/jQuiz/jQuiz/Models/Clue.swift
+++ Week 09/jQuiz/jQuiz/Models/Clue.swift
@@ -9,9 +9,70 @@
 import Foundation
 
 struct Clue {
-
+  let id: Int
+  let answer: String
+  let question: String
+  let value: Int
+  let airdate: String
+  let createdAt: String
+  let updatedAt: String
+  let categoryid: Int
+  let gameid: JSONNull?
+  let invalidCount: JSONNull?
+  let category: Category
+  
+  enum CodingKeys: String, CodingKey {
+    case id = "id"
+    case answer = "answer"
+    case question = "question"
+    case value = "value"
+    case airdate = "airdate"
+    case createdAt = "created_at"
+    case updatedAt = "updated_at"
+    case categoryid = "category_id"
+    case gameid = "game_id"
+    case invalidCount = "invalid_count"
+    case category = "category"
+  }
 }
 
 struct Category {
+  let id: Int
+  let title: String
+  let createdAt: String
+  let updatedAt: String
+  let cluesCount: Int
+  
+  enum CodingKeys: String, CodingKey {
+    case id = "id"
+    case title = "title"
+    case createdAt = "created_at"
+    case updatedAt = "updated_at"
+    case cluesCount = "clues_count"
+  }
+}
 
+
+class JSONNull: Codable, Hashable {
+  
+  public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+    return true
+  }
+  
+  public init() {}
+  
+  func hash(into hasher: inout Hasher) {
+  }
+  
+  public required init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    if !container.decodeNil() {
+      throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+    }
+  }
+  
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encodeNil()
+  }
 }
