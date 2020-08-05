37049a06660a1bd03847181ba14babd5f4256ec5
Bartolome Estelrich <tom@estelrichapplabs.com>
2020-08-05 10:34:48 -0400

    Project files created
    AnimationFactory project created and code folders structured.

---
+++ Week 11/AnimationFactory/AnimationFactory/Controllers/ViewController.swift
@@ -0,0 +1,13 @@
+// 2020.08.05 | AnimationFactory - ViewController.swift | Copyright © 2020 BEstelrich. All rights reserved.
+import UIKit
+
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+}
+
