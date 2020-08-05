e66e548e54710d055d81478f202387b8d8c4a093
Bartolome Estelrich <tom@estelrichapplabs.com>
2020-08-05 12:14:11 -0400

    Menu buttons behaviour completed
    Menu buttons have been created, connected to the view-controller and properly animated to the app expected behaviour.

--- Week 11/AnimationFactory/AnimationFactory/Controllers/ViewController.swift
+++ Week 11/AnimationFactory/AnimationFactory/Controllers/ViewController.swift
@@ -4,10 +4,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var homeButton: UIButton!
+  @IBOutlet weak var changePositionOffsetConstraint: NSLayoutConstraint!
+  @IBOutlet weak var changeColorOffsetConstraint: NSLayoutConstraint!
+  @IBOutlet weak var changeShapeOffsetConstraint: NSLayoutConstraint!
+  
+  private var isMenuOpened = false {
+    didSet {
+      isMenuOpened ? homeButton.setImage(UIImage(named: "Play.png"), for: .normal) : homeButton.setImage(UIImage(named: "Home.png"), for: .normal)
+    }
+  }
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
-
+  
+  
+  @IBAction func homeButtonDidTap(_ sender: UIButton) {
+    isMenuOpened.toggle()
+    animateMenuButtons()
+    
+  }
+  
+  func animateMenuButtons() {
+    UIView.animate(
+      withDuration: 1/3,
+      delay: 0,
+      options: .curveEaseIn,
+      animations: {
+        self.changePositionOffsetConstraint.constant = self.isMenuOpened ? 15 : -60
+        self.changeColorOffsetConstraint.constant = self.isMenuOpened ? 15 : -60
+        self.changeShapeOffsetConstraint.constant = self.isMenuOpened ? 15 : -60
+        self.view.layoutIfNeeded()
+      }
+    )
+  }
+  
 }
-
