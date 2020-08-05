1893dacb76558c21aa7e946234ee24366615e940
Bartolome Estelrich <tom@estelrichapplabs.com>
2020-08-05 17:42:44 -0400

    Animations added
    Added an animation for each button and the logic to make them work simultaneously.

--- Week 11/AnimationFactory/AnimationFactory/Controllers/ViewController.swift
+++ Week 11/AnimationFactory/AnimationFactory/Controllers/ViewController.swift
@@ -5,40 +5,114 @@ import UIKit
 class ViewController: UIViewController {
 
   @IBOutlet weak var homeButton: UIButton!
-  @IBOutlet weak var changePositionOffsetConstraint: NSLayoutConstraint!
-  @IBOutlet weak var changeColorOffsetConstraint: NSLayoutConstraint!
-  @IBOutlet weak var changeShapeOffsetConstraint: NSLayoutConstraint!
+  @IBOutlet weak var animatedView: UIView!
+  @IBOutlet var homeButtonOffsetArrayOfConstraints: [NSLayoutConstraint]!
   
+  private let propertyAnimator = UIViewPropertyAnimator(duration: 2, curve: .linear)
+
   private var isMenuOpened = false {
     didSet {
       isMenuOpened ? homeButton.setImage(UIImage(named: "Play.png"), for: .normal) : homeButton.setImage(UIImage(named: "Home.png"), for: .normal)
     }
   }
   
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
   }
   
   
   @IBAction func homeButtonDidTap(_ sender: UIButton) {
-    isMenuOpened.toggle()
     animateMenuButtons()
-    
+    startAnimations()
   }
   
+  
+  @IBAction func changePositionButtonDidTap(_ sender: UIButton) {
+    addTransformingAnimation()
+    showAnimationNotification()
+  }
+  
+  
+  @IBAction func changeColorButtonDidTap(_ sender: UIButton) {
+    addAppearanceAnimation()
+    showAnimationNotification()
+  }
+  
+  
+  @IBAction func changeShapeButtonDidTap(_ sender: UIButton) {
+    addShapeAnimation()
+    showAnimationNotification()
+  }
+
+  
   func animateMenuButtons() {
+    isMenuOpened.toggle()
     UIView.animate(
       withDuration: 1/3,
       delay: 0,
       options: .curveEaseIn,
       animations: {
-        self.changePositionOffsetConstraint.constant = self.isMenuOpened ? 15 : -60
-        self.changeColorOffsetConstraint.constant = self.isMenuOpened ? 15 : -60
-        self.changeShapeOffsetConstraint.constant = self.isMenuOpened ? 15 : -60
+        self.homeButtonOffsetArrayOfConstraints.forEach { constraint in
+          constraint.constant = self.isMenuOpened ? 15 : -60
+        }
         self.view.layoutIfNeeded()
       }
     )
   }
   
+  
+  func startAnimations() {
+    if !isMenuOpened {
+      propertyAnimator.startAnimation()
+      
+      propertyAnimator.addCompletion { _ in
+        let reversePropertyAnimator = UIViewPropertyAnimator(duration: 2, curve: .linear)
+        reversePropertyAnimator.addAnimations {
+          self.resetAnimatedView()
+        }
+        reversePropertyAnimator.startAnimation()
+      }
+    }
+  }
+  
+  
+  func resetAnimatedView() {
+    self.animatedView.transform = .identity
+    self.animatedView.backgroundColor = .systemGreen
+    self.animatedView.alpha = 1
+    self.animatedView.layer.cornerRadius = 0
+  }
+  
+  
+  func addTransformingAnimation() {
+    propertyAnimator.addAnimations {
+      let transform = CGAffineTransform(translationX: 100, y: 150).rotated(by: .pi/2).scaledBy(x: 1.5, y: 1.5)
+      self.animatedView.transform = transform
+    }
+  }
+  
+  
+  func addAppearanceAnimation() {
+    propertyAnimator.addAnimations {
+      self.animatedView.backgroundColor = UIColor.systemRed
+      self.animatedView.alpha = 0.5
+    }
+    showAnimationNotification()
+  }
+  
+  
+  func addShapeAnimation() {
+    propertyAnimator.addAnimations {
+      self.animatedView.layer.cornerRadius = 50
+    }
+    showAnimationNotification()
+  }
+  
+  
+  func showAnimationNotification() {
+    print("Notification added to the queue.")
+  }
+  
 }
+
